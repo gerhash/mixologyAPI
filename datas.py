5bf571a70863b9d8e8ea72c775e02f09b9217a22
G#sh <62940515+gerhash@users.noreply.github.com>
2024-06-13 17:09:43 +0200

    frontend

--- datas.py
+++ datas.py
@@ -36,111 +36,12 @@ cocktails = [
             {"name": "soda", "quantity": 10, "scale": "ml"},
         ],
         "tutorial": '''
-          Place a sugar cube in an old fashioned glass and sprinkle it with two drops of angostura. Make a splash of soda and crush with the muddler until the sugar is completely dissolved. Add ice until the glass is filled, add the whiskey and mix. Stir with a bar spoon and decorate the glass with a slice of orange after leaving its essence on the edge of the glass. Place a maraschino cherry in the glass and serve the Old Fashioned cocktail.
-      ''',
+        Place a sugar cube in an old fashioned glass and sprinkle it with two drops of angostura. Make a splash of soda and crush with the muddler until the sugar is completely dissolved. Add ice until the glass is filled, add the whiskey and mix. Stir with a bar spoon and decorate the glass with a slice of orange after leaving its essence on the edge of the glass. Place a maraschino cherry in the glass and serve the Old Fashioned cocktail.
+    ''',
         "story": '''
-          The history of the Old Fashioned cocktail begins at the Waldorf-Astoria hotel, a well-known luxury hotel in New York. Colonel James Pepper was a regular at the Waldorf and was a member of the Pendennis Club of Louisville. Here he had tasted a drink very similar to the Old Fashioned, invented for him who loved old-fashioned whiskey. Once he arrived at Astoria, he taught the barman the recipe they had created for him at Pendennis, giving life to a cocktail that made history.
-      ''',
+        The history of the Old Fashioned cocktail begins at the Waldorf-Astoria hotel, a well-known luxury hotel in New York. Colonel James Pepper was a regular at the Waldorf and was a member of the Pendennis Club of Louisville. Here he had tasted a drink very similar to the Old Fashioned, invented for him who loved old-fashioned whiskey. Once he arrived at Astoria, he taught the barman the recipe they had created for him at Pendennis, giving life to a cocktail that made history.
+    ''',
         "difficulty": "Easy",
     },
-    {
-        "id": 1,
-        "name": "Old Fashioned",
-        "img": "/assets/1.jpg",
-        "ingredients": [
-            {"name": "whisky", "quantity": 40, "scale": "ml"},
-            {"name": "sugar", "quantity": 5, "scale": "g"},
-            {"name": "anguish", "quantity": 0.1, "scale": "ml"},
-            {"name": "orange", "quantity": 150, "scale": "g"},
-            {"name": "cherry", "quantity": 6, "scale": "g"},
-            {"name": "soda", "quantity": 10, "scale": "ml"},
-        ],
-        "tutorial": '''
-          Place a sugar cube in an old fashioned glass and sprinkle it with two drops of angostura. Make a splash of soda and crush with the muddler until the sugar is completely dissolved. Add ice until the glass is filled, add the whiskey and mix. Stir with a bar spoon and decorate the glass with a slice of orange after leaving its essence on the edge of the glass. Place a maraschino cherry in the glass and serve the Old Fashioned cocktail.
-      ''',
-        "story": '''
-          The history of the Old Fashioned cocktail begins at the Waldorf-Astoria hotel, a well-known luxury hotel in New York. Colonel James Pepper was a regular at the Waldorf and was a member of the Pendennis Club of Louisville. Here he had tasted a drink very similar to the Old Fashioned, invented for him who loved old-fashioned whiskey. Once he arrived at Astoria, he taught the barman the recipe they had created for him at Pendennis, giving life to a cocktail that made history.
-      ''',
-        "difficulty": "Easy",
-    },
-    {
-        "id": 1,
-        "name": "Old Fashioned",
-        "img": "/assets/1.jpg",
-        "ingredients": [
-            {"name": "whisky", "quantity": 40, "scale": "ml"},
-            {"name": "sugar", "quantity": 5, "scale": "g"},
-            {"name": "anguish", "quantity": 0.1, "scale": "ml"},
-            {"name": "orange", "quantity": 150, "scale": "g"},
-            {"name": "cherry", "quantity": 6, "scale": "g"},
-            {"name": "soda", "quantity": 10, "scale": "ml"},
-        ],
-        "tutorial": '''
-          Place a sugar cube in an old fashioned glass and sprinkle it with two drops of angostura. Make a splash of soda and crush with the muddler until the sugar is completely dissolved. Add ice until the glass is filled, add the whiskey and mix. Stir with a bar spoon and decorate the glass with a slice of orange after leaving its essence on the edge of the glass. Place a maraschino cherry in the glass and serve the Old Fashioned cocktail.
-      ''',
-        "story": '''
-          The history of the Old Fashioned cocktail begins at the Waldorf-Astoria hotel, a well-known luxury hotel in New York. Colonel James Pepper was a regular at the Waldorf and was a member of the Pendennis Club of Louisville. Here he had tasted a drink very similar to the Old Fashioned, invented for him who loved old-fashioned whiskey. Once he arrived at Astoria, he taught the barman the recipe they had created for him at Pendennis, giving life to a cocktail that made history.
-      ''',
-        "difficulty": "Easy",
-    },
-    {
-        "id": 1,
-        "name": "Old Fashioned",
-        "img": "/assets/1.jpg",
-        "ingredients": [
-            {"name": "whisky", "quantity": 40, "scale": "ml"},
-            {"name": "sugar", "quantity": 5, "scale": "g"},
-            {"name": "anguish", "quantity": 0.1, "scale": "ml"},
-            {"name": "orange", "quantity": 150, "scale": "g"},
-            {"name": "cherry", "quantity": 6, "scale": "g"},
-            {"name": "soda", "quantity": 10, "scale": "ml"},
-        ],
-        "tutorial": '''
-          Place a sugar cube in an old fashioned glass and sprinkle it with two drops of angostura. Make a splash of soda and crush with the muddler until the sugar is completely dissolved. Add ice until the glass is filled, add the whiskey and mix. Stir with a bar spoon and decorate the glass with a slice of orange after leaving its essence on the edge of the glass. Place a maraschino cherry in the glass and serve the Old Fashioned cocktail.
-      ''',
-        "story": '''
-          The history of the Old Fashioned cocktail begins at the Waldorf-Astoria hotel, a well-known luxury hotel in New York. Colonel James Pepper was a regular at the Waldorf and was a member of the Pendennis Club of Louisville. Here he had tasted a drink very similar to the Old Fashioned, invented for him who loved old-fashioned whiskey. Once he arrived at Astoria, he taught the barman the recipe they had created for him at Pendennis, giving life to a cocktail that made history.
-      ''',
-        "difficulty": "Easy",
-    },
-    {
-        "id": 1,
-        "name": "Old Fashioned",
-        "img": "/assets/1.jpg",
-        "ingredients": [
-            {"name": "whisky", "quantity": 40, "scale": "ml"},
-            {"name": "sugar", "quantity": 5, "scale": "g"},
-            {"name": "anguish", "quantity": 0.1, "scale": "ml"},
-            {"name": "orange", "quantity": 150, "scale": "g"},
-            {"name": "cherry", "quantity": 6, "scale": "g"},
-            {"name": "soda", "quantity": 10, "scale": "ml"},
-        ],
-        "tutorial": '''
-          Place a sugar cube in an old fashioned glass and sprinkle it with two drops of angostura. Make a splash of soda and crush with the muddler until the sugar is completely dissolved. Add ice until the glass is filled, add the whiskey and mix. Stir with a bar spoon and decorate the glass with a slice of orange after leaving its essence on the edge of the glass. Place a maraschino cherry in the glass and serve the Old Fashioned cocktail.
-      ''',
-        "story": '''
-          The history of the Old Fashioned cocktail begins at the Waldorf-Astoria hotel, a well-known luxury hotel in New York. Colonel James Pepper was a regular at the Waldorf and was a member of the Pendennis Club of Louisville. Here he had tasted a drink very similar to the Old Fashioned, invented for him who loved old-fashioned whiskey. Once he arrived at Astoria, he taught the barman the recipe they had created for him at Pendennis, giving life to a cocktail that made history.
-      ''',
-        "difficulty": "Easy",
-    },
-    {
-        "id": 1,
-        "name": "Old Fashioned",
-        "img": "/assets/1.jpg",
-        "ingredients": [
-            {"name": "whisky", "quantity": 40, "scale": "ml"},
-            {"name": "sugar", "quantity": 5, "scale": "g"},
-            {"name": "anguish", "quantity": 0.1, "scale": "ml"},
-            {"name": "orange", "quantity": 150, "scale": "g"},
-            {"name": "cherry", "quantity": 6, "scale": "g"},
-            {"name": "soda", "quantity": 10, "scale": "ml"},
-        ],
-        "tutorial": '''
-          Place a sugar cube in an old fashioned glass and sprinkle it with two drops of angostura. Make a splash of soda and crush with the muddler until the sugar is completely dissolved. Add ice until the glass is filled, add the whiskey and mix. Stir with a bar spoon and decorate the glass with a slice of orange after leaving its essence on the edge of the glass. Place a maraschino cherry in the glass and serve the Old Fashioned cocktail.
-      ''',
-        "story": '''
-          The history of the Old Fashioned cocktail begins at the Waldorf-Astoria hotel, a well-known luxury hotel in New York. Colonel James Pepper was a regular at the Waldorf and was a member of the Pendennis Club of Louisville. Here he had tasted a drink very similar to the Old Fashioned, invented for him who loved old-fashioned whiskey. Once he arrived at Astoria, he taught the barman the recipe they had created for him at Pendennis, giving life to a cocktail that made history.
-      ''',
-        "difficulty": "Easy",
-    },
+
 ]
